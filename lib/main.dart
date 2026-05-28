bec1fc39e322da54e74fe69daa0137a8dc2b21b7
Youssef <youssefadly332@gmail.com>
2026-05-28 15:10:52 +0300

    define the name of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const PlantLife());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PlantLife extends StatelessWidget {
+  const PlantLife({super.key});
 
   // This widget is the root of your application.
   @override
